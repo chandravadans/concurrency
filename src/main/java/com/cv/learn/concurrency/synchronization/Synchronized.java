29279ad05e364ab4f9831c9521550d9c48abbfaa
Chandravadan <s.chandravadan@gmail.com>
2016-12-29 11:03:22 +0530

    demoing interleaved executions

---
+++ src/main/java/com/cv/learn/concurrency/synchronization/Synchronized.java
@@ -0,0 +1,37 @@
+package com.cv.learn.concurrency.synchronization;
+
+public class Synchronized {
+    private int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Synchronized s = new Synchronized();
+        s.doStuff();
+
+    }
+
+    public void doStuff() throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++)
+                count++;
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++)
+                count++;
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Count is " + count);
+        if (count == 200000) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
