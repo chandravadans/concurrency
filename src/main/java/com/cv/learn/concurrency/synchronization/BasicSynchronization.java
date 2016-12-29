806d92b0a831f8826cecfe41aff2a7dbbc8737d5
Chandravadan <s.chandravadan@gmail.com>
2016-12-29 10:36:31 +0530

    A simple mechanism to stop a running thread

--- src/main/java/com/cv/learn/concurrency/synchronization/BasicSynchronization.java
+++ src/main/java/com/cv/learn/concurrency/synchronization/BasicSynchronization.java
@@ -1,9 +1,12 @@
 package com.cv.learn.concurrency.synchronization;
 
+import java.util.Scanner;
+
 class Processor extends Thread {
+    private boolean running = true;
     @Override
     public void run() {
-        while (true) {
+        while (running) {
             System.out.println("Processor running");
             try {
                 Thread.sleep(250);
@@ -11,13 +14,23 @@ class Processor extends Thread {
                 e.printStackTrace();
             }
         }
+    }
 
+    public void setRunning(boolean running) {
+        this.running = running;
     }
 }
 
 public class BasicSynchronization {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Processor processor = new Processor();
         processor.start();
+
+        System.out.println("Paused main thread. Press return to resume and stop running thread");
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+
+        System.out.println("Stopping thread");
+        processor.setRunning(false);
     }
 }
